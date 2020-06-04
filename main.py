a16d4b3400fdbeb324ae809d174655360a818c93
Francis Dolbec <fdolbec123@gmail.com>
2020-06-04 10:14:18 -0400

    Bouton Quitter implémenté.

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from PyQt5.QtWidgets import *
 class Menu(QMainWindow):
     def __init__(self):
         super(Menu, self).__init__()
-        self.bouton = QPushButton(self)
+        self.bouton_quitter = QPushButton(self)
         self.label = QLabel(self)
         self.init_ui()
         self.setGeometry(100, 100, 1280, 720)
         self.setWindowTitle("Bienvenue dans SkipBo!")
 
-    def clicked(self):
-        print("cliqué")
-        self.label.setText("cliqué")
-        self.label.repaint()
+    def quitter(self):
+        self.close()
+        # self.label.setText("cliqué")
+        # self.label.repaint()
 
     def init_ui(self):
         self.label.setText("Coucou")
         self.label.move(100, 100)
-        self.bouton.setText("Clique!")
-        self.bouton.move(200, 200)
-        self.bouton.clicked.connect(self.clicked)
+        self.bouton_quitter.setText("Quitter")
+        self.bouton_quitter.move(200, 200)
+        self.bouton_quitter.clicked.connect(self.quitter)
 
 
 def main():
