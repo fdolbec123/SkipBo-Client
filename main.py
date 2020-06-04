9dd0068355a5d6f064812d2c5104a18ec44af237
Francis Dolbec <fdolbec123@gmail.com>
2020-06-04 15:00:11 -0400

    Prototype 1 de l'écran d'accueil.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QRect
-from PyQt5.QtGui import QRegion
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QRegion, QPalette, QPixmap, QBrush
 # import sys
 
 
 class Menu(QMainWindow):
     def __init__(self):
         super(Menu, self).__init__()
+        self.background_picture = QGraphicsView()
+        self.scene = QGraphicsScene()
+        self.grp = QGroupBox("Mon Groupe")
+        self.palette = QPalette()
+        self.proxy = QGraphicsProxyWidget()
+        self.background_picture2 = QPixmap()
+        # self.setStyleSheet("background-color: yellow;")
         self.bouton_creer = QPushButton(self)
         self.bouton_rejoindre = QPushButton(self)
         self.bouton_quitter = QPushButton(self)
         self.bouton_help = QPushButton(self)
         self.label = QLabel(self)
         self.init_ui()
-        self.setWindowTitle("Bienvenue dans SkipBo!")
+        self.setWindowTitle("Bienvenue dans Skip-Bo!")
         self.hauteur = int
         self.quart_hauteur = int
         self.longueur = int
@@ -25,13 +32,36 @@ class Menu(QMainWindow):
         self.close()
 
     def init_ui(self):
+        # --------------- Paramètres de la fenêtre --------------------
         self.setGeometry(100, 100, 1280, 720)
+        self.background_picture2.load('background_menu2.png')
+        self.palette.setBrush(self.backgroundRole(), QBrush(self.background_picture2))
+        self.setPalette(self.palette)
+        # self.background_picture.setStyleSheet("background-image: url(background_menu.png);")
+        # self.setCentralWidget(self.background_picture)
+        # self.background_picture.setScene(self.scene)
+        # # --------------- Fin des paramètres -------------------------
+        #
+        # # --------------- Paramètres pour le groupbox ------------------
+        # self.grp.setLayout(QVBoxLayout())
+        # self.grp.layout().addWidget(self.bouton_help)
+        # self.grp.layout().addWidget(self.bouton_creer)
+        # self.grp.layout().addWidget(self.bouton_rejoindre)
+        # self.grp.layout().addWidget(self.bouton_quitter)
+        # self.palette.setColor(QPalette.Background, Qt.transparent)
+        # self.grp.setPalette(self.palette)
+        # self.proxy.setWidget(self.grp)
+        # self.scene.addItem(self.proxy)
+        # --------------- Fin des paramètres -------------------------
+
+        # --------------- Éléments présents dans la fenêtre ----------
         self.hauteur = int(self.frameGeometry().height())
         self.quart_hauteur = int(self.hauteur / 4)
         self.longueur = int(self.frameGeometry().width())
         self.quart_longueur = int(self.longueur/4)
-        self.label.setText("Coucou")
-        self.label.move(100, 100)
+        self.label.setText("* Skip-Bo est une marque déposée de Mattel Inc.")
+        self.label.move(20, 690)
+        self.label.adjustSize()
         self.bouton_creer.setText("Créer une partie")
         self.bouton_creer.setGeometry((self.quart_longueur-160), (3*self.quart_hauteur), self.quart_longueur,
                                       int(self.quart_hauteur / 2))
