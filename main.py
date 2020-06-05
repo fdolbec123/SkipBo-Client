1b4ea61c0aca832794a895e31919c7ae9ac3251c
Francis Dolbec <fdolbec123@gmail.com>
2020-06-05 17:06:53 -0400

    Refont complete. Reste à implémenté une message box pour le bouton help.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QRegion, QPalette, QPixmap, QBrush
+from PyQt5.QtGui import QRegion, QPalette, QPixmap, QBrush, QIcon
 import creation_partie
 import verification
 import join
@@ -35,6 +35,10 @@ class Menu(QMainWindow):
         self.rsp3 = None
         self.join = QDialog()
         self.verification = QDialog()
+        self.quitter_icon = QIcon
+        self.join_icon = QIcon
+        self.creer_icon = QIcon
+        self.help_icon = QIcon
 
     def center(self):
         frame = self.frameGeometry()
@@ -73,31 +77,19 @@ class Menu(QMainWindow):
         else:
             print("Cancel")
 
+    def help(self):
+        print("help")
+        pass
+
     def init_ui(self):
         # --------------- Paramètres de la fenêtre --------------------
         self.resize(1280, 720)
         self.setMaximumSize(1280, 720)
         self.setMinimumSize(1280, 720)
         self.center()
-        self.background_picture2.load('background_menu3.png')
+        self.background_picture2.load('backgroundmenu3.png')
         self.palette.setBrush(self.backgroundRole(), QBrush(self.background_picture2))
         self.setPalette(self.palette)
-        # self.background_picture.setStyleSheet("background-image: url(background_menu.png);")
-        # self.setCentralWidget(self.background_picture)
-        # self.background_picture.setScene(self.scene)
-        # # --------------- Fin des paramètres -------------------------
-        #
-        # # --------------- Paramètres pour le groupbox ------------------
-        # self.grp.setLayout(QVBoxLayout())
-        # self.grp.layout().addWidget(self.bouton_help)
-        # self.grp.layout().addWidget(self.bouton_creer)
-        # self.grp.layout().addWidget(self.bouton_rejoindre)
-        # self.grp.layout().addWidget(self.bouton_quitter)
-        # self.palette.setColor(QPalette.Background, Qt.transparent)
-        # self.grp.setPalette(self.palette)
-        # self.proxy.setWidget(self.grp)
-        # self.scene.addItem(self.proxy)
-        # --------------- Fin des paramètres -------------------------
 
         # --------------- Éléments présents dans la fenêtre ----------
         self.hauteur = int(self.frameGeometry().height())
@@ -107,24 +99,37 @@ class Menu(QMainWindow):
         self.label.setText("* Skip-Bo est une marque déposée de Mattel Inc.")
         self.label.move(20, 690)
         self.label.adjustSize()
-        self.bouton_creer.setText("Créer une partie")
+        self.bouton_creer.setText("")
+        self.creer_icon = QIcon("bouton_creer.png")
+        self.bouton_creer.setIcon(self.creer_icon)
+        self.bouton_creer.setIconSize(QSize(320, 90))
         self.bouton_creer.setGeometry((self.quart_longueur-160), (3*self.quart_hauteur), self.quart_longueur,
                                       int(self.quart_hauteur / 2))
         # self.bouton_creer.setStyleSheet("QPushButton{background: transparent;}")
         self.bouton_creer.clicked.connect(self.creer_nouvelle_partie)
-        self.bouton_rejoindre.setText("Rejoindre une partie")
+        self.bouton_rejoindre.setText("")
+        self.join_icon = QIcon("bouton_join.png")
+        self.bouton_rejoindre.setIcon(self.join_icon)
+        self.bouton_rejoindre.setIconSize(QSize(318, 88))
         self.bouton_rejoindre.setGeometry(((2*self.quart_longueur)-160), (3*self.quart_hauteur), self.quart_longueur,
                                           int(self.quart_hauteur / 2))
         self.bouton_rejoindre.clicked.connect(self.joindre_partie)
-        self.bouton_help.setText("?")
+        self.bouton_help.setText("")
+        self.help_icon = QIcon("bouton_help.png")
+        self.bouton_help.setIcon(self.help_icon)
+        self.bouton_help.setIconSize(QSize(65, 65))
         self.bouton_help.setFixedHeight(100)
         self.bouton_help.setFixedWidth(100)
         self.bouton_help.move(self.longueur-int(self.quart_longueur / (4/2)), -10)
+        self.bouton_help.clicked.connect(self.help)
         self.test_rect = QRect(20, 20, 60, 60)
         self.test_region = QRegion(self.test_rect, QRegion.Ellipse)
         self.test_region.boundingRect().size()
         self.bouton_help.setMask(self.test_region)
-        self.bouton_quitter.setText("Quitter")
+        self.bouton_quitter.setText("")
+        self.quitter_icon = QIcon("bouton_quitter.png")
+        self.bouton_quitter.setIcon(self.quitter_icon)
+        self.bouton_quitter.setIconSize(QSize(318, 88))
         self.bouton_quitter.setGeometry(((3*self.quart_longueur)-160), (3*self.quart_hauteur), self.quart_longueur,
                                         int(self.quart_hauteur / 2))
         self.bouton_quitter.clicked.connect(self.quitter)
